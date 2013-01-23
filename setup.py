993731b038ff9f5b4b56dc711915c60bf890a18f
Filip Wasilewski <en@ig.ma>
2013-01-23 16:57:06 +0100

    Reformat setup file

--- setup.py
+++ setup.py
@@ -3,26 +3,27 @@
 
 from setuptools import setup
 
+
 version = "0.2.post1"
 
 setup(
-    name = "dj-cmd",
-    version = version,
-    description = "`dj cmd` is a Django shortcut command.",
-    license = "BSD",
+    name="dj-cmd",
+    version=version,
+    description="`dj cmd` is a Django shortcut command.",
+    license="BSD",
 
-    author = "Filip Wasilewski",
-    author_email = "en@ig.ma",
+    author="Filip Wasilewski",
+    author_email="en@ig.ma",
 
-    url = "https://github.com/nigma/dj-cmd",
-    download_url = "https://github.com/nigma/dj-cmd/zipball/master",
+    url="https://github.com/nigma/dj-cmd",
+    download_url="https://github.com/nigma/dj-cmd/zipball/master",
 
-    long_description = open("README.rst").read(),
+    long_description=open("README.rst").read(),
 
-    packages = ["dj_cmd"],
-    package_dir = {"dj_cmd": "src"},
-    include_package_data = True,
-    classifiers = (
+    packages=["dj_cmd"],
+    package_dir={"dj_cmd": "src"},
+    include_package_data=True,
+    classifiers=(
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Environment :: Web Environment",
@@ -38,8 +39,8 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ),
-    tests_require = ["django>=1.3"],
-    entry_points = {
+    tests_require=["django>=1.3"],
+    entry_points={
         "console_scripts": [
             "dj = dj_cmd.dj:main",
         ]
